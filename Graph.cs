cfd5f6c9b8f25ed26202a6bfa57577544a2e18d6
myrovh <myrovh@yandex.com>
2015-08-10 17:54:37 +1000

    fixed grid generating too large

--- Graph.cs
+++ Graph.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public class Graph : MonoBehaviour
 {
     public float XGridBounds = 8;
-    public float YGridBounds = 8;
-    public float ZGridBounds = 3;
+    public float ZGridBounds = 8;
+    public float YGridBounds = 3;
     public float GridInterval = 1;
     public GameObject WaypointPrefab;
 
@@ -16,28 +16,27 @@ public class Graph : MonoBehaviour
 
     public void BuildGraph()
     {
-        Debug.Log("Graph Generation Starting");
         int nodeCount = 0;
-        for (float z = ZGridBounds; z > -ZGridBounds; z -= GridInterval)
+        for (float y = YGridBounds; y >= 0; y -= GridInterval)
         {
-            for (float x = XGridBounds; x > -XGridBounds; x -= GridInterval)
+            for (float x = XGridBounds; x >= 0 ; x -= GridInterval)
             {
-                for (float y = YGridBounds; y > -YGridBounds; y -= GridInterval)
+                for (float z = ZGridBounds; z >= 0; z -= GridInterval)
                 {
                     nodeCount++;
                     GameObject tempObject =
-                        (GameObject) Instantiate(WaypointPrefab, new Vector3(x, z, y), Quaternion.identity);
+                        (GameObject) Instantiate(WaypointPrefab, new Vector3(x, y, z), Quaternion.identity);
                     tempObject.name = "node_" + nodeCount;
                     tempObject.transform.parent = gameObject.transform;
                 }
             }
         }
-        Debug.Log("Graph Generation Ending");
+        Debug.Log("Graph Generation Finished");
     }
 
-    void OnDrawGizmos()
+    private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(transform.position, new Vector3(XGridBounds * GridInterval, YGridBounds * GridInterval, ZGridBounds * GridInterval));
+        Gizmos.DrawWireCube(transform.position + new Vector3(XGridBounds / 2.0f, YGridBounds / 2.0f, ZGridBounds / 2.0f), new Vector3(XGridBounds, YGridBounds, ZGridBounds));
     }
 }
--- Node.cs
+++ Node.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Node : MonoBehaviour
 {
     public float GizmoDrawRadius = 0.5f;
-    void OnDrawGizmos()
+    void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, GizmoDrawRadius);
